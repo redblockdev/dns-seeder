e04d3c1ebe31e5c19a628ceddf521f01132ce846
redblockdev <redblockdev@gmail.com>
2023-09-04 05:53:53 -0700

    P2P Port to 6211

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 18335 : 8335;
+    return testnet ? 17335 : 6211;
 }
 
 //
